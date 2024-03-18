675bb292d6451d5022616856759909f32dc2907d
bukka5sandhya <bukkapatnamsandhya920@gmail.com>
2024-03-18 20:12:00 +0530

    add user name and password on ui

---
+++ src/components/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {} from '../componentsCss/details.css';
+
+function Details() {
+  return (
+    <div className='detailsContent'>
+      <p>user name : anvesh</p>
+      <p>password : anvesh123</p>
+    </div>
+  )
+}
+
+export default Details
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Form from './Form'
 import { } from 'bootstrap'
+import Details from './Details'
 
 function Login() {
   return (
     <>
       <h1>Login</h1>
+      <Details/>
       <Form/>
     </>
   )
